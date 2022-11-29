3d5ec7e440c54464e87bc6f96b6bfebdd15368e6
Georg Bonin <114496506+GeBon22@users.noreply.github.com>
2022-11-29 16:30:13 +0100

    added button and h3

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,16 @@ export default function HomePage() {
   return (
     <div>
       <StyledBody>
-        <h3>This comes from the index.js</h3>
+        <h3>Wo drückt der Schuh?</h3>
+        <StyledButton>Online bestellen und kaufen</StyledButton>
+        <StyledButton>Widerruf und Retoure</StyledButton>
       </StyledBody>
     </div>
   );
 }
 
 const StyledBody = styled.div`
-  display: flex;
+  display: grid;
   justify-content: center;
   align-items: center;
   background-color: #f3e8e8;
@@ -20,7 +22,14 @@ const StyledBody = styled.div`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   width: 90vw;
   height: 75vh;
-  gap: 20px;
   margin-top: 10px;
   margin-bottom: 10px;
 `;
+
+const StyledButton = styled.button`
+color: #572887;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  height: 30vh;
+  width: 80vw;
+`
