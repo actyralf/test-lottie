9333858c1fff13cf2d5974e88527d485847af949
Georg Bonin <114496506+GeBon22@users.noreply.github.com>
2022-11-29 17:03:51 +0100

    added animation files

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
 import styled from "styled-components";
 import Link from "next/link";
+import lottie from "lottie-web";
+import {useEffect, useRef} from "react";
 
 export default function HomePage() {
+
+  const container = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      render: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("../public/BestellungAnimation"),
+    });
+  }, []);
+
+
   return (
     <div>
       <StyledBody>
         <h3>Wo drückt der Schuh?</h3>
-        <Link href="/bestellung"><StyledButton>Online bestellen und kaufen</StyledButton></Link>
-        <Link href="/widerruf"><StyledButton>Widerruf und Retoure</StyledButton></Link>
+        <Link href="/bestellung">
+       {/*  <BestellungAnimation href={container}></BestellungAnimation> */}
+          <StyledButton>Online bestellen und kaufen</StyledButton>
+        </Link>
+        <Link href="/widerruf">
+          <StyledButton>Widerruf und Retoure</StyledButton>
+        </Link>
       </StyledBody>
     </div>
   );
@@ -28,9 +49,21 @@ const StyledBody = styled.div`
 `;
 
 const StyledButton = styled.button`
-color: #572887;
+  color: #572887;
+  font-size: 18px;
+  background-color: #E6CEEE;
   border-radius: 10px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);;
   height: 30vh;
   width: 80vw;
-`
+`;
+
+/* const BestellungAnimation = styled.section`
+  width: 70px;
+  height: 90px;
+`; */
+
+
+/* const WiderrufAnimation = styled.section`
+
+`; */
