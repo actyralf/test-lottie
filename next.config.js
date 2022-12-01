8b0ad9707f7071cf3486a0cbd7b99d0e2e2d0a98
Ralf Siewert <ralf.siewert@actyvyst.com>
2022-12-01 15:17:11 +0100

    remove babel dependency

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  compiler: {
-    styledComponents: true,
-  },
-  eslint: {
-    dirs: ["pages", "components", "styles"],
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
